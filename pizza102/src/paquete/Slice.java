c2a746cab76fe6723aff11b4897a3c9bb52f66e5
Retard <pabloqwe1000@gmail.com>
2019-02-12 16:44:04 +0100

    Auto stash before merge of "master" and "origin/master"

--- pizza102/src/paquete/Slice.java
+++ pizza102/src/paquete/Slice.java
@@ -1,5 +1,28 @@
 package paquete;
 
 public class Slice {
+<<<<<<< Updated upstream
 
+=======
+	
+	public static void slice(int coordX, int coordY, int[][]matriz, int T, int C, int min, int max) { //CAMBIAR RETURN TYPE
+		//if(!Condiciones) => Cortar matriz
+			if(matriz.length >= matriz[0].length) { //Corte horizontal
+				
+				int[] tomates = new int[matriz.length];
+				for(int i=0; i<matriz.length; i++) { //Comprueba
+					for(int j=0; j<matriz[0].length; j++) {
+						if(matriz[i][j]==1)
+							tomates[i]++;
+					}
+				}
+				
+			}
+			else if(matriz.length < matriz[0].length) { //Corte vertical
+				
+			}
+		
+	}
+	
+>>>>>>> Stashed changes
 }
