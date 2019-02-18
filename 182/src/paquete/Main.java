623caf82a9439c17b1aa6adf412f7e66db8269e3
adri <100383242@alumnos.uc3m.es>
2019-02-18 16:50:46 +0100

    en proceso

--- 182/src/paquete/Main.java
+++ 182/src/paquete/Main.java
@@ -1,16 +1,68 @@
 package paquete;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-
+	public static int rows;
+	public static int colum;
+	public static int num_drones;
+	public static int turns_maxima;
+	public static int carga_maxima;
+	public static int carga_tipo[];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stuba
+		
 
 	}
 	
 	public static int distancia(int xi, int yi, int xf, int yf) {
-		return Math.abs(Math.sqrt(Math.pow(xi-xf, 2)+Math.pow(yi-yf, 2)));
+		return Math.abs(Math.sqrt((xi-xf)+(yi-yf)));
+	}
+	
+	
+	public static void entrada() throws IOException {
+		//input
+		String archivo_pruebas="Pruebas/a_example.in";
+		//input
+				
+		FileReader fichero = new FileReader(archivo_pruebas);
+		BufferedReader br = new BufferedReader(fichero);
+				
+		String linea=br.readLine();
+		String[]datos=linea.split(" ");
+				
+		
+		rows=Integer.parseInt(datos[0]);
+		colum=Integer.parseInt(datos[1]);
+		num_drones=Integer.parseInt(datos[2]);
+		turns_maxima=Integer.parseInt(datos[3]);
+		carga_maxima=Integer.parseInt(datos[4]);
+		
+		linea=br.readLine();
+		carga_tipo=new int[Integer.parseInt(linea)];
+		
+		//pesos por tipo
+		linea=br.readLine();
+		datos=linea.split(" ");
+		
+		//meter cargas
+		for(int i=0;i<carga_tipo.length;i++) {
+			carga_tipo[i]=Integer.parseInt(datos[i]);
+		}
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
 	}
 
 }
