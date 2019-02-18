3f0cca51d266460dbe5c3f6b1ca23afba651f2ff
caldito <100383255@alumnos.uc3m.es>
2019-02-18 16:26:21 +0100

    empiezo distancia

--- 182/src/paquete/Main.java
+++ 182/src/paquete/Main.java
@@ -1,10 +1,16 @@
 package paquete;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stuba
 
+	}
+	
+	public static int distancia(int xi, int yi, int xf, int yf) {
+		return Math.abs(Math.sqrt((xi-xf)+(yi-yf)));
 	}
 
 }
